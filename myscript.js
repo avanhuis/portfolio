0011360dbd083fb20fe21f579a09ba1e7d66db5f
Amanda Milke <amandalmilke@gmail.com>
2016-01-29 11:39:42 -0500

    First Commit

---
+++ myscript.js
@@ -0,0 +1,6 @@
+/* Function will return First Name and Last Name and add a message */
+function myFunction() {
+				var x = document.getElementById("fname").value;
+				var y = document.getElementById("lname").value;
+				document.getElementById("demo").innerHTML = "Hi " + x + " " + y + "!" + " Thank you for your message. I will get back to you within 24 hours!";
+				}
